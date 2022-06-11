04f28e8b0b70ea5ffe191e8c2ec57e4b1753ef6c
Qui_Belua <qui.belua@gmail.com>
2022-06-11 16:22:28 +0200

    add solution unique paths

---
+++ unique_paths/unique_paths.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] *n for _ in range(m)]
+        print(dp)
+        
+        for col in range(1,m):
+            for row in range(1,n):
+                dp[col][row] = dp[col-1][row] + dp[col][row-1]
+        print(dp)
+        return dp[m-1][n-1]
